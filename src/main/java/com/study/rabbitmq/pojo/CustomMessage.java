754429b5d0d9aa6439f8eb47cf168a0bfa5ffe30
Vijay Pallampadi Sasidharan <30817858+vijayps-undefined53@users.noreply.github.com>
2025-06-30 16:44:55 +0530

    second commit

--- src/main/java/com/study/rabbitmq/pojo/CustomMessage.java
+++ src/main/java/com/study/rabbitmq/pojo/CustomMessage.java
@@ -1,22 +1,9 @@
 package com.study.rabbitmq.pojo;
 
+import lombok.Data;
+
+@Data
 public class CustomMessage {
     private String text;
     private String priority;
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
